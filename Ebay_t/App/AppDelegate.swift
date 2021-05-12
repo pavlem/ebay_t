16761ad8d0fbf4c4fdfb43d57e0c87141b751a00
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-05-12 08:34:02 +0200

    Initial Setup

---
+++ Ebay_t/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ebay_t
+//
+//  Created by Pavle Mijatovic on 12.5.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
